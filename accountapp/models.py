24f4591b6bc9bc0af762f5e805b0e532649daec9
Yuuraa <yoorachoi8937@gmail.com>
2021-08-03 16:46:59 +0900

    Add migration

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class HelloWorld(models.Model):
+    text = models.CharField(max_length=255, null=False) #null을 사용해 이 attribute가 없어도 되는지 여부 지정
