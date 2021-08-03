a8ff95243ee2052d43724386a6838b96635b044d
Yuuraa <yoorachoi8937@gmail.com>
2021-08-03 17:08:01 +0900

    Save POST data to DB

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+from accountapp.models import HelloWorld
 
 # 브라우저에서 어떤 경로로 접속했을 때 보일 html 페이지를 만들 것
-
 def hello_world(request):
     if request.method == "POST":
-        return render(request, 'accountapp/hello_world.html', context={'text': 'TEXT CONTEXT HAHA'})
+        temp = request.POST.get('hello_world_input')
+
+        new_hello_data = HelloWorld()
+        new_hello_data.text = temp
+        new_hello_data.save() # DB에 객체 저장
+
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_data})
+
     return render(request, 'accountapp/hello_world.html')
