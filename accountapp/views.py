9bab0e40ba22f456a3d3bd2df073dc5e816fdeb9
Yuuraa <yoorachoi8937@gmail.com>
2021-08-03 16:58:11 +0900

    Add post method handling

--- accountapp/views.py
+++ accountapp/views.py
@@ -6,5 +6,6 @@ from django.http import HttpResponse
 # 브라우저에서 어떤 경로로 접속했을 때 보일 html 페이지를 만들 것
 
 def hello_world(request):
-    # return HttpResponse("Hell world!")
+    if request.method == "POST":
+        return render(request, 'accountapp/hello_world.html', context={'text': 'TEXT CONTEXT HAHA'})
     return render(request, 'accountapp/hello_world.html')
